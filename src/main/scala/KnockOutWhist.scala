75785c8abb5c8a6ad00e8952f67b60eb18f167d0
Janis <janis-e@gmx.de>
2024-10-11 09:09:17 +0200

    Modified Table - Karten + Ablage Side by Side

--- src/main/scala/KnockOutWhist.scala
+++ src/main/scala/KnockOutWhist.scala
@@ -1,5 +1,5 @@
 
-object H {
+object Main {
 
   class KnockOutWhist {
 
@@ -8,28 +8,32 @@ object H {
   val eol = sys.props("line.separator")
 
   def bar(cellWidth: Int = 9, cellNum: Int = 1) =
-      (("-" * cellWidth) * cellNum) + eol
+    ("-" * cellWidth) * cellNum
 
-    def cells(cellWidth: Int = 7, cellNum: Int = 1) =
-      ("|" + " " * cellWidth) * cellNum + "|" + eol
+  def cells(cellWidth: Int = 7, cellNum: Int = 1) =
+    ("|" + " " * cellWidth) * cellNum + "|"
 
-    def meshk =
-      bar() + cells() + "|Karten | " + eol + cells() * 2 + bar()
+  def meshk =
+    bar() + cells() + eol + "|Karten | " + eol + (cells()+ eol) * 2 + bar() + eol
 
-    def mesha =
-      bar() + cells() + "|Ablage | " + eol + cells() * 2 + bar()
+  def mesha =
+    bar() + cells() + eol + "|Ablage | " + eol + (cells()+ eol) * 2 + bar() + eol
 
-    def mesht1 =
-      bar() + cells() + "|Trumpfe| " + eol + "|s1     |\n" + cells() + bar()
+  def mesht1 =
+    bar() + eol + cells() + eol + "|Trumpfe| " + eol + "|s1     |\n" + cells() + eol + bar() + eol
 
-    def mesht2 =
-      bar() + cells() + "|Trumpfe|" + eol + "|s2     |\n" + cells() + bar()
+  def mesht2 =
+    bar() + eol + cells() + eol + "|Trumpfe|" + eol + "|s2     |\n" + cells() + eol + bar() + eol
+  def meshTable = {
+    bar() + "\t" + bar() + eol + cells() + "\t" + cells() + eol +
+      "|Karten |" + "\t" + "|Ablage |" + eol + (cells() + "\t" + cells() + eol) * 2 +
+      bar() + "\t" + bar() + eol
+  }
 
   def main(args: Array[String]): Unit = {
     println("Welcome to Knock out Whist")
 
-    print(meshk)
-    print(mesha)
+    print(meshTable)
     print(mesht1)
     print(mesht2)
   }
