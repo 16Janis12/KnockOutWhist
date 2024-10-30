556305e1b775da2772f34e7b344acd633ce6405c
Janis <janis-e@gmx.de>
2024-10-30 18:17:53 +0100

    Added an additional method for retrieving the hand of a Player

--- src/main/scala/de/knockoutwhist/cards/Player.scala
+++ src/main/scala/de/knockoutwhist/cards/Player.scala
@@ -2,17 +2,15 @@ package de.knockoutwhist.cards
 
 case class Player(name: String) {
   private var hand: Option[Hand] = None
+
+  def currentHand(): Option[Hand] = hand
   var doglife: Boolean = false
   def provideHand(hand: Hand): Boolean = {
     this.hand = Some(hand)
     true
   }
   def pickTrumpsuit(): Suit = {
-    
     Suit.Spades
-    
-    
-    
   }
   
   
