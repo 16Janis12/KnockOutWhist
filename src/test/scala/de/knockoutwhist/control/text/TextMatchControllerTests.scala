fc3bc8ebbf08d800253d0743720d2defcbeba6ff
LQ63 <lkhermann@web.de>
2024-11-08 09:53:28 +0100

    Added TextMatchControllerTests Class

---
+++ src/test/scala/de/knockoutwhist/control/text/TextMatchControllerTests.scala
@@ -0,0 +1,32 @@
+package de.knockoutwhist.control.text
+
+import de.knockoutwhist.KnockOutWhist
+import de.knockoutwhist.cards.Suit.Hearts
+import de.knockoutwhist.cards.{Card, Suit}
+import de.knockoutwhist.player.Player
+import de.knockoutwhist.rounds.{Match, Round}
+import de.knockoutwhist.testutils.TestUtil
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.matchers.should.Matchers.{should, shouldBe}
+import org.scalatest.wordspec.AnyWordSpec
+
+class TextMatchControllerTests extends AnyWordSpec with Matchers {
+//  "The matchcontrol" should {
+////    "Start a match when 1 is the input to start()" in {
+////      TextMatchControl.start()
+////      val hi = TestUtil.simulateInput("1\n")
+////      Console.equals("Starting new match.")
+////    }
+//    "be able to create a new round" in {
+//      val player1 = Player("Gunter")
+//      val player2 = Player("Peter")
+//      val players = List(player1, player2)
+//      val match1 = Match(players)
+//      val round = TestUtil.simulateInput("1\n") {
+//        TextMatchControl.nextRound(match1)
+//      }
+//         round.trumpSuit shouldBe Suit.Hearts
+//
+//    }
+//  }
+}
