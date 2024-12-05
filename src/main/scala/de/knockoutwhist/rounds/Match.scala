1311cb20b84cd7c67da17dc561cc91cf7baaaf8d
Janis <janis-e@gmx.de>
2024-12-05 19:26:56 +0100

    Match immutable

--- src/main/scala/de/knockoutwhist/rounds/Match.scala
+++ src/main/scala/de/knockoutwhist/rounds/Match.scala
@@ -1,16 +1,31 @@
 package de.knockoutwhist.rounds
 
 import de.knockoutwhist.player.AbstractPlayer
+import de.knockoutwhist.utils.CustomPlayerQueue
 import de.knockoutwhist.utils.Implicits.*
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.immutable.List
+import scala.util.Random
 
-case class Match(totalplayers: List[AbstractPlayer], var numberofcards: Int = 7) {
+case class Match(totalplayers: List[AbstractPlayer], var numberofcards: Int = 7, startingPlayer: Int = -1, dogLife: Boolean = false, roundlist: List[Round] = List[Round]()) {
 
-  val roundlist: ListBuffer[Round] = ListBuffer[Round]()
-  var current_round: Option[Round] = None
-  var dogLife = false
-
+  val playerQueue: CustomPlayerQueue[AbstractPlayer] = CustomPlayerQueue[AbstractPlayer](
+    totalplayers.toArray,
+    (startingPlayer == -1) ? Random.nextInt(totalplayers.length) |: startingPlayer
+  )
+  
+  def addRound(round: Round): Match = {
+    this.copy(startingPlayer = playerQueue.currentIndex,roundlist = roundlist :+ round)
+  }
+  
+  def setDogLife(): Match = {
+    this.copy(startingPlayer = playerQueue.currentIndex, dogLife = true)
+  }
+  
+  def updatePlayers(totalplayers: List[AbstractPlayer]): Match = {
+    this.copy(startingPlayer = playerQueue.currentIndex, totalplayers = totalplayers)
+  }
+  
   override def toString: String = {
     s"$totalplayers, $numberofcards"
   }
