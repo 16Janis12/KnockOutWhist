786204dfafa148da68a4e27af5c3e17198e97a14
Janis <janis.e.20@gmx.de>
2025-10-19 20:49:25 +0200

    Rename PlayCardEvent to RequestCardEvent for clarity in event handling

--- src/main/scala/de/knockoutwhist/events/player/RequestCardEvent.scala
+++ src/main/scala/de/knockoutwhist/events/player/RequestCardEvent.scala
@@ -2,6 +2,6 @@ package de.knockoutwhist.events.player
 
 import de.knockoutwhist.player.AbstractPlayer
 
-case class PlayCardEvent(player: AbstractPlayer) extends PlayerEvent(player) {
+case class RequestCardEvent(player: AbstractPlayer) extends PlayerEvent(player) {
   override def id: String = "PlayCardEvent"
 }
