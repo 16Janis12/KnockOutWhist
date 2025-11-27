ef7397f7f1312e0d87ab7ccaab94159d7f7e9773
Janis <janis-e@gmx.de>
2025-11-27 09:53:14 +0100

    fix(api): fixes (#58)
    Reviewed-on: https://git.janis-eccarius.de/KnockOutWhist/KnockOutWhist/pulls/58

--- src/main/scala/de/knockoutwhist/events/global/RoundEndEvent.scala
+++ src/main/scala/de/knockoutwhist/events/global/RoundEndEvent.scala
@@ -4,5 +4,5 @@ import de.knockoutwhist.player.AbstractPlayer
 import de.knockoutwhist.utils.events.SimpleEvent
 
 case class RoundEndEvent(winner: AbstractPlayer, amountOfTricks: Int) extends SimpleEvent {
-  override def id: String = s"RoundEndEvent()"
+  override def id: String = s"RoundEndEvent"
 }
